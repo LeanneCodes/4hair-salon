65f2d13f928dcbbb11cc1c1ef10a0855e0a4e05d
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2021-08-30 13:30:27 +0000

    add introductory message to the user explaining what they need to do

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,6 +13,19 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('4hair-salon')
 
-sales = SHEET.worksheet('Sales')
-data = sales.get_all_values()
-print(data)
+
+def weekly_sales_input():
+    """
+    This function asks the user for the weekly sales
+    per product, in a specific order.
+    """
+    print("Please enter your weekly sales figures for each product in the order below:")
+    print("Shampoo, Conditioner, Styling Gel, Hair Oil, Mousse, Combs, Brushes and Hair Accessories\n")
+    print("If you had no sales for that product in the week, please type 0\n")
+    print("Data should be 8 numbers, separated by commas")
+    print("For example: 8,11,3,16,0,29,7,4\n")
+
+    data_string = input("Enter your numbers here: \n")
+    print(f"The data provided is {data_string}")
+
+weekly_sales_input()
