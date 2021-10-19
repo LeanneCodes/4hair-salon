6723a198bdd21a14e087265899589daa93777650
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2021-10-19 11:00:00 +0000

    adjust line spacing so it doesn't exceed 79 characters

--- run.py
+++ run.py
@@ -22,8 +22,9 @@ def weekly_data_input():
     """
     while True:
         print("\nWelcome to the 4Hair Salon National Sales Tracking System!\n")
-        print("Please enter your weekly sales figures for each city in the order below:")
-        print("London, Bristol, Manchester, Birmingham, Liverpool and Nottingham\n")
+        print("Enter your weekly sales figures in the order below:")
+        print("London, Bristol, Manchester, Birmingham," +
+              " Liverpool and Nottingham\n")
         print("Data should be 6 numbers, separated by commas")
         print("For example: 1000,250,3456,780,90,0\n")
         sales_string = input("Enter your numbers here: \n")
@@ -37,7 +38,8 @@ def weekly_data_input():
             print("\nData is valid!\n")
             print("Please confirm the data you entered is correct?")
             while True:
-                user_confirm = (input("(1)Yes or (2)No \nType the number of your response: \n"))
+                user_confirm = (input("(1)Yes or (2)No." +
+                                      " Type the number of your response: \n"))
                 if user_confirm == "1":
                     print("\nThe system will proceed...\n")
                     return sales_data, booking_data
@@ -61,7 +63,8 @@ def validate_data(value1, value2):
         [int(value) for value in value2]
         if len(value1) != 6 or len(value2) != 6:
             raise ValueError(
-                f"Exactly 6 values are required for both separated by commas.\nYou provided {len(value1)} and {len(value2)}"
+                "Exactly 6 values are required for both separated by commas." +
+                f"\nYou provided {len(value1)} and {len(value2)}"
             )
     except ValueError as e:
         print(f"\nInvalid data: {e}, please try again.\n")
@@ -103,7 +106,7 @@ def update_abv_worksheet(abv):
     Updates the abv worksheet and appends a new row of data.
     """
     abv_results = SHEET.worksheet("ABV").append_row(abv)
-    print("Your average booking value for each city has been updated successfully!\n")
+    print("Your ABV for each city has been updated successfully!\n")
 
     return abv_results
 
@@ -127,7 +130,7 @@ def abv_reccos(data):
     each city. Depending on the performance, recommendations
     are suggested to help improve performance.
     """
-    print("Recommendations to improve average booking value per city are below, if any...\n")
+    print("Recommendations to improve ABV per city are below, if any...\n")
 
     abv_value = []
     for column in data:
@@ -146,25 +149,43 @@ def abv_reccos(data):
     }
 
     for key, value in abv_dict.items():
-        print(f"{value[0]}'s Regional ABV Target is {value[1]} and your latest ABV is currently at {abv_value[key]}.")
+        print(f"{value[0]}'s Regional ABV Target is {value[1]} and your" +
+              " latest ABV is currently at {abv_value[key]}.")
         if abv_value[key] < (0.1 * value[1]):
-            print("This location is severely underperforming.\nTry offering more add-on services, such as:\n1. Trimming Services\n2. Hair/Root Colouring\n3. Highlights\n4. Balayage/Foilayage\n5. Blowout\n6. Bridal Hair\n7. 4Hair's Luxury Hair Care Kits\n")
+            print("This location is severely underperforming.\nTry" +
+                  " offering more add-on services, such as:\n1. Trimming" +
+                  " Services\n2. Hair/Root Colouring\n3. Highlights\n4." +
+                  " Balayage/Foilayage\n5. Blowout\n6. Bridal Hair\n7." +
+                  " 4Hair's Luxury Hair Care Kits\n")
         elif abv_value[key] < (0.3 * value[1]):
-            print("Your average booking value for this location is dangerously low.\nTry upselling your clients with services that are in the £25-£40 range.\n")
+            print("Your average booking value for this location is " +
+                  "dangerously low.\nTry upselling your clients with" +
+                  " services that are in the £25-£40 range.\n")
         elif abv_value[key] < (0.5 * value[1]):
-            print("This location is more than 50% under target.\nEncourage clients to trial the latest hairstyle trends that are £30+.\n")
+            print("This location is more than 50% under target.\nEncourage" +
+                  " clients to trial the latest hairstyle trends that" +
+                  " are £30+.\n")
         elif abv_value[key] < (0.7 * value[1]):
-            print("This city's salon is currently falling behind.\nAim to sell 4Hair's luxury hair care kits to each client to exceed targets.\n")
+            print("This city's salon is currently falling behind.\nAim to" +
+                  " sell 4Hair's luxury hair care kits to each client to " +
+                  "exceed targets.\n")
         elif abv_value[key] < (0.95 * value[1]):
-            print("So close to target, but not quite there yet.\nEncourage clients to buy the hair oil serums and masks after every appointment.\n")
+            print("So close to target, but not quite there yet.\nEncourage" +
+                  " clients to buy the hair oil serums and masks after " +
+                  "every appointment.\n")
         elif abv_value[key] <= (0.99 * value[1]):
-            print("This salon is doing really well.\nIncentivise employees with 45% off £50+ treatments for themselves or family, only if they exceed target next week.\n")
+            print("This salon is doing really well.\nIncentivise " +
+                  "employees with 45% off £50+ treatments for themselves or" +
+                  " family, only if they exceed target next week.\n")
         elif abv_value[key] > value[1]:
-            print("Congratulations! This city is smashing target goals. Keep it up!\n")
+            print("Congratulations! This city is smashing target goals." +
+                  " Keep it up!\n")
         else:
-            print("This salon is meeting target.\nHowever, aim to increase the salon's ABV to prevent falling behind.\n")
+            print("This salon is meeting target.\nHowever, aim to" +
+                  " increase the salon's ABV to prevent falling behind.\n")
 
-    print("To get access to the 4Hair Salon Google worksheet, please copy this link here:\nhttps://bit.ly/4hair-salon\n")
+    print("To get access to the 4Hair Salon Google worksheet, please copy " +
+          "this link here:\nhttps://bit.ly/4hair-salon\n")
 
     return abv_value
 
